6f2fe70983ad3de36852c6c95e1e828db341760f
Md. Ramiz <sramiz052@gmail.com>
2023-02-28 21:10:20 +0530

    Yani alemdar

---
+++ about.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong rel=stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Yani Alemdar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron">
+  <h1>YA-NI ALEMDAR :)</h1>
+  <p>WELCOME TO MY PORTFOLIO.</p>
+</div>
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About us</h2>
+  </div>
+   <div class="container-fluid">
+         <div class="row">
+           <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/img9.jpg" class="image-fluid aboutimg">
+     </div>
+   <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">I Am Yani Alemdar</h2>
+      <p class="py-3">Join me on my wonderful journey being a housewife and a mom.</p>
+      <a href=" https://www.blogger.com/profile/01704547071004423388" class="btn btn-success">Let's explore</a>
+     
+    </div>
+  </div>
+</div>
+</section>
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@Sanober Choudhary mini project</p>
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title></title>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong rel=stylesheet">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="https://my.readymag.com/edit/4071266/2/">Yani Alemdar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://mail.google.com/mail/u/0/#inbox?compose=lqrsljczlkjtCqlqfvzPDnKFpCRxZdQhPplqJCprtFsGXMpNZGkLFpTWKdvfJvJpDZjrgSRwnRqSdBBsz">Contact</a>
+      </li>
+      
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/img4.jpg" alt="USA" width="1100" height="500">
+      <div class="carousel-caption">
+        <h2>USA</h2>
+        <p>We had such a great time in USA!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/img72.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>Thank you, New York!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/img6.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Happy Housewife!!</h3>
+        <p>Blogging and Exploring!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">About us</h2>
+  </div>
+   <div class="container-fluid">
+         <div class="row">
+           <div class="col-lg-6 col-md-6 col-12">
+    <img src="images/img75.jpg" class="image-fluid aboutimg">
+     </div>
+   <div class="col-lg-6 col-md-6 col-12">
+    <h2 class="display-4">I Am Yani Alemdar</h2>
+      <p class="py-3">Join me on my wonderful journey being a housewife and a mom.</p>
+      <a href=" https://www.instagram.com/p/CbTKtFmpivp/?next=%2F" class="btn btn-success">Let's explore</a>
+     
+    </div>
+  </div>
+</div>
+</section>
+
+<section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">YANI ALEMDAR</h2>
+  </div>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" >
+  <img class="card-img-top" src="images/img21.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Yani Alemdar</h4>
+    <p class="card-text">Glimpse of Youtube Shorts!!</p>
+    <a href="https://www.youtube.com/shorts/TtzTQSyPi0o" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" >
+  <img class="card-img-top" src="images/img20.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Yani Alemdar</h4>
+    <p class="card-text">Linkedin Account below!!</p>
+    <a href="https://www.linkedin.com/in/yani-alemdar-387b4a1a4/?originalSubdomain=in" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+      </div>
+      
+      <div class="col-lg-4 col-md-4 col-12">
+      <div class="card" >
+  <img class="card-img-top" src="images/img22.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">Yani Alemdar</h4>
+    <p class="card-text">Instagram Account below!!</p>
+    <a href="https://www.instagram.com/yani_alemdar/?next=%2F" class="btn btn-primary">See Profile</a>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+      </section>
+
+      <section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">OUR GALLERY</h2>
+  </div>
+     <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img35.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img38.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img37.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img31.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img56.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img54.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img33.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img29.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img26.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img51.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img40.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img23.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img42.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img47.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img28.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img24.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img48.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img45.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img46.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img41.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img44.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img34.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img30.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img39.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img52.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img53.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img50.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img27.jpeg" class="img-fluid pb-3">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+          <img src="images/img25.jpeg" class="img-fluid pb-3">
+        </div>
+      </div>
+     </div>  
+     
+     <section class="my-5">
+  <div class="py-5">
+    <h2 class="text-center">FEEDBACK</h2>
+  </div>
+  <div class="w-50 m-auto">
+    <form action="userinfo.php" method="post">
+      <div class="form-group">
+        <label>Username</label>
+        <input type="text" name="user" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Email-Id</label>
+        <input type="text" name="email" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Mobile No.</label>
+        <input type="text" name="mobile" autocomplete="off" class="form-control">
+      </div>
+      <div class="form-group">
+        <label>Comments</label>
+        
+        <textarea name="comments" class="form-control" >
+        </textarea>
+        
+      </div>
+      <button type="submit" class="btn btn-success">Submit</button>
+
+    </form>
+  </div>
+
+</section>
+
+<footer>
+  <p class="p-3 bg-dark text-white text-center">@Sanober Choudhary mini project</p>
+</footer>
+ 
+  
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    
+</body>
+</html>
